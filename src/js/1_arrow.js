00f87024410a236999a8a72c7cdca872714ee96b
Yara <yara.gordillo@gmail.com>
2022-10-25 19:42:40 +0200

    more ideas on js arrow actions

--- src/js/1_arrow.js
+++ src/js/1_arrow.js
@@ -3,13 +3,15 @@
 //Constantes y elementos DOM
 const arrowUp = document.querySelector('.js-arrow-up');
 const arrowDown = document.querySelector('.js-arrow-down');
-const formFieldset = document.querySelector('.js-fieldset');
+const formFieldsetFill = document.querySelector('.js-fieldset-fill');
 
 //Funciones
 function open() {
   arrowDown.classList.add('collapsed');
   arrowUp.classList.remove('collapsed');
   formFieldset.classList.remove('collapsed');
+  //Añadir que si las otras dos secciones fieldset design y fieldset share no tienen la clase collapse se les debe añadir
+  //para que solo haya abierta una seccion at a time. Si la tienen entonces no se hace nada
 }
 
 function close() {
--- src/js/2_inputs.js
+++
@@ -1,1 +0,0 @@
-//Constantes y elementos DOM
