5faf6dfe1bfbfc320bae8e1688376b44573c2a8b
Yara <yara.gordillo@gmail.com>
2022-10-30 20:10:35 +0100

    last changes on js

---
+++ src/js/3_fill-Inputs.js
@@ -0,0 +1,10 @@
+//Elementos HTML del DOM
+const inputsInfo = document.querySelectorAll('.js_input');
+//const previewName = document.querySelector('.js_preview_name');
+const previewJob = document.querySelector('.js_preview_job');
+const previewImg = document.querySelector('.js_preview_img');
+//const previewIcons = document.querySelectorAll('.js_card_icon');
+
+//Functions
+
+//Events
