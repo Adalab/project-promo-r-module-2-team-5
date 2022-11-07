0cc41565d2885de60fb674f30549560cffee2483
Yara <yara.gordillo@gmail.com>
2022-11-07 09:18:03 +0100

    refactoring inputs js

--- src/js/3_fill-Inputs.js
+++ src/js/3_fill-Inputs.js
@@ -1,31 +1,34 @@
-'use strict';//descomentarlo al final
+'use strict'; //descomentarlo al final
 
 //Elementos HTML del DOM
 const form = document.querySelector('.js_form');
-const previewCard = {
-    palette: '',
-    name: '',
-    job: '',
-    image: '',
-    email: '',
-    phone: '',
-    linkedin: '',
-    gitHub: ''
+/*const previewName ya declarada en design-form*/
+const previewJob = document.querySelector('.js_preview_job');
+const previewImage = document.querySelector('.js_preview_picture');
+const previewLinks = document.querySelectorAll('.js_preview_link');
 
+const previewCard = {
+  palette: '',
+  name: '',
+  job: '',
+  image: '',
+  email: '',
+  phone: '',
+  linkedin: '',
+  github: '',
 };
-//const previewName declarada en design-form
-const previewJob = document.querySelector(".js_preview_job");
-const previewImage = document.querySelector(".js_preview_picture");
-const previewLinks = document.querySelectorAll(".js_preview_link");
 
 //Functions
-
 function handleInputForm(event) {
-   
-    const inputName = event.target.name;
-    const inputValue = event.target.value;
+  const inputName = event.target.name;
+  const inputValue = event.target.value;
 
-    /*if(inputName === 'name') {
+  //Forma corta para rellenar el objeto
+  previewCard[inputName] = inputValue;
+  renderCard();
+
+  //Forma larga para rellenar el objeto
+  /*if(inputName === 'name') {
         previewCard.name = inputValue;
     } else if (inputName === 'job') {
         previewCard.job = inputValue;
@@ -40,46 +43,47 @@ function handleInputForm(event) {
     } else if (inputName === 'gitHub') {
         previewCard.gitHub = inputValue;
     } */ //forma larga
-    
-    
-
-    previewCard[inputName] = inputValue; //forma rápida //para rellenar el objeto
-
-    renderCard();
 }
-    function renderCard() {
-        
-        if(previewCard.name === "") {
-            previewName.innerHTML = 'Nombre Apellido';
-        } else {
-            previewName.innerHTML = previewCard.name;
-        }
-        if(previewCard.job === "") {
-            previewJob.innerHTML = 'Front-end developer';
-        } else {
-            previewJob.innerHTML = previewCard.job;
-        }
-        if(previewCard.image === "") {
-            previewImage.style = "background-image:url(./assets/images/astronaut.jpg)";
-        } else {
-            previewImage.style = previewCard.image;
-        }
-        
-        
-        for(const link of previewLinks) {
-            if(link.id === 'phone') {
-                link.href = previewCard.phone;
-            } else if (link.id === 'email') {
-                link.href = previewCard.email;
-            } else if (link.id === 'linkedin') {
-                link.href = previewCard.linkedin;
-            } else if (link.id === 'github') {
-                link.href = previewCard.github;
-            }
-        }
-    }
 
-    
+function renderCard() {
+  let name = '';
+  let job = '';
+  let img = '';
+  let links = [];
+
+  if (previewCard.name === '') {
+    name = 'Nombre Apellido';
+  } else {
+    name = previewCard.name;
+  }
+  if (previewCard.job === '') {
+    job = 'Front-end developer';
+  } else {
+    job = previewCard.job;
+  }
+  if (previewCard.image === '') {
+    img = 'background-image:url(./assets/images/astronaut.jpg)';
+  } else {
+    img = previewCard.image;
+  }
+
+  for (let i = 0; i < previewLinks.length; i++) {
+    if (previewLinks[i].id === 'phone') {
+      links = previewCard.phone;
+    } else if (previewLinks[i].id === 'email') {
+      links = previewCard.email;
+    } else if (previewLinks[i].id === 'linkedin') {
+      links = previewCard.linkedin;
+    } else if (previewLinks[i].id === 'github') {
+      links = previewCard.github;
+    }
+    previewLinks[i].href = links[i];
+  }
+
+  previewName.innerHTML = name;
+  previewJob.innerHTML = job;
+  previewImage.style = img;
+}
 
 //Events
 form.addEventListener('input', handleInputForm);
