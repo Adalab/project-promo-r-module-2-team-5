0f1c00c885539569ea64cb5c3d415c7db2b8ca98
Yara <yara.gordillo@gmail.com>
2022-11-07 09:45:20 +0100

    fix js inputs

--- src/js/3_fill-Inputs.js
+++ src/js/3_fill-Inputs.js
@@ -1,31 +1,29 @@
-'use strict';//descomentarlo al final
+'use strict'; //descomentarlo al final
 
 //Elementos HTML del DOM
 const form = document.querySelector('.js_form');
 const previewCard = {
-    palette: '',
-    name: '',
-    job: '',
-    image: '',
-    email: '',
-    phone: '',
-    linkedin: '',
-    gitHub: ''
-
+  palette: '',
+  name: '',
+  job: '',
+  image: '',
+  email: '',
+  phone: '',
+  linkedin: '',
+  github: '',
 };
 //const previewName declarada en design-form
-const previewJob = document.querySelector(".js_preview_job");
-const previewImage = document.querySelector(".js_preview_picture");
-const previewLinks = document.querySelectorAll(".js_preview_link");
+const previewJob = document.querySelector('.js_preview_job');
+const previewImage = document.querySelector('.js_preview_picture');
+const previewLinks = document.querySelectorAll('.js_preview_link');
 
 //Functions
 
 function handleInputForm(event) {
-   
-    const inputName = event.target.name;
-    const inputValue = event.target.value;
+  const inputName = event.target.name;
+  const inputValue = event.target.value;
 
-    /*if(inputName === 'name') {
+  /*if(inputName === 'name') {
         previewCard.name = inputValue;
     } else if (inputName === 'job') {
         previewCard.job = inputValue;
@@ -40,46 +38,40 @@ function handleInputForm(event) {
     } else if (inputName === 'gitHub') {
         previewCard.gitHub = inputValue;
     } */ //forma larga
-    
-    
 
-    previewCard[inputName] = inputValue; //forma rápida //para rellenar el objeto
+  previewCard[inputName] = inputValue; //forma rápida //para rellenar el objeto
 
-    renderCard();
+  renderCard();
 }
-    function renderCard() {
-        
-        if(previewCard.name === "") {
-            previewName.innerHTML = 'Nombre Apellido';
-        } else {
-            previewName.innerHTML = previewCard.name;
-        }
-        if(previewCard.job === "") {
-            previewJob.innerHTML = 'Front-end developer';
-        } else {
-            previewJob.innerHTML = previewCard.job;
-        }
-        if(previewCard.image === "") {
-            previewImage.style = "background-image:url(./assets/images/astronaut.jpg)";
-        } else {
-            previewImage.style = previewCard.image;
-        }
-        
-        
-        for(const link of previewLinks) {
-            if(link.id === 'phone') {
-                link.href = previewCard.phone;
-            } else if (link.id === 'email') {
-                link.href = previewCard.email;
-            } else if (link.id === 'linkedin') {
-                link.href = previewCard.linkedin;
-            } else if (link.id === 'github') {
-                link.href = previewCard.github;
-            }
-        }
-    }
+function renderCard() {
+  if (previewCard.name === '') {
+    previewName.innerHTML = 'Nombre Apellido';
+  } else {
+    previewName.innerHTML = previewCard.name;
+  }
+  if (previewCard.job === '') {
+    previewJob.innerHTML = 'Front-end developer';
+  } else {
+    previewJob.innerHTML = previewCard.job;
+  }
+  if (previewCard.image === '') {
+    previewImage.style = 'background-image:url(./assets/images/astronaut.jpg)';
+  } else {
+    previewImage.style = previewCard.image;
+  }
 
-    
+  for (const link of previewLinks) {
+    if (link.id === 'phone') {
+      link.href = previewCard.phone;
+    } else if (link.id === 'email') {
+      link.href = previewCard.email;
+    } else if (link.id === 'linkedin') {
+      link.href = previewCard.linkedin;
+    } else if (link.id === 'github') {
+      link.href = previewCard.github;
+    }
+  }
+}
 
 //Events
 form.addEventListener('input', handleInputForm);
