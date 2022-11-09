9e08c7e8972ebaf7526b39dd4c3beb806398f3b2
Monica Hidalgo Martin <monicahidalgomartin@gmail.com>
2022-11-09 17:18:46 +0100

    minimal changes

--- src/js/3_fill-Inputs.js
+++ src/js/3_fill-Inputs.js
@@ -1,4 +1,4 @@
-'use strict'; //descomentarlo al final
+
 
 //Elementos HTML del DOM
 const form = document.querySelector('.js_form');
@@ -17,10 +17,8 @@ const previewCard = {
   linkedin: '',
   github: '',
 };
-//const previewName declarada en design-form
-const previewJob = document.querySelector('.js_preview_job');
-const previewImage = document.querySelector('.js_preview_picture');
-const previewLinks = document.querySelectorAll('.js_preview_link');
+
+
 
 //Functions
 function handleInputForm(event) {
